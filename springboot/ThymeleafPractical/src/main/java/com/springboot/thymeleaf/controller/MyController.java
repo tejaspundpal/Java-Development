0613093330e5ae0c7d521756723f9dba4d1ae82d
tejaspundpal <96496782+tejaspundpal@users.noreply.github.com>
2024-06-18 18:02:17 +0530

    thymeleaf basic

--- springboot/ThymeleafPractical/src/main/java/com/springboot/thymeleaf/controller/MyController.java
+++ springboot/ThymeleafPractical/src/main/java/com/springboot/thymeleaf/controller/MyController.java
@@ -1,12 +1,33 @@
-package controller;
+package com.springboot.thymeleaf.controller;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
 	
 	@RequestMapping(value="/about",method=RequestMethod.GET)
-	public String about() {
+	public String about(Model model) {
 		System.out.println("inside about handler");
+		model.addAttribute("name", "Tejas Pundpal");
+		model.addAttribute("currentDate", new Date().toLocaleString());
+		
+		List<String> names = List.of("Tejas","TP","Tejya","TTP");
+		model.addAttribute("names", names);
+
+		model.addAttribute("isActive", false);
+		model.addAttribute("gender", "F");
+		
 		return "about";
 		//about.html
+	}
+
+	@RequestMapping(value="/fragment",method=RequestMethod.GET)
+	public String fragment(Model model) {
+		return "fragment";
 	}
 }
